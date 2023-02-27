470201a4cfc28b2bcba9209dbf557f03e07b0dc0
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-02-27 17:19:51 +0900

    02.27.17시

---
+++ workspace/memberServlet/src/main/java/member/dao/MemberDAO.java
@@ -0,0 +1,95 @@
+package member.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import member.bean.MemberDTO;
+
+public class MemberDAO {
+   private Connection conn;
+   private PreparedStatement pstmt;
+   private ResultSet rs;
+
+   private String driver = "oracle.jdbc.driver.OracleDriver";
+   private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+   private String username = "c##java";
+   private String password = "1234";
+   
+   private static MemberDAO memberDAO = new MemberDAO();
+
+   public static MemberDAO getInstance() {
+
+      return memberDAO;
+   }
+
+   public MemberDAO() {
+      try {
+         Class.forName(driver); // Class타입으로 생성
+         System.out.println("driver loading 성공");
+      } catch (ClassNotFoundException e) {
+
+         e.printStackTrace();
+      }
+      // 접속은 한번만 하는것이 아니기 때문에 생성자에서 하면 안됨.
+   }
+
+   public void getConnection() {
+      try {
+         conn = DriverManager.getConnection(url, username, password);/* 오라클 드라이버 */
+         System.out.println("connection 성공");
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public int memberWrite(MemberDTO memberDTO) {
+      int su = 0;
+
+      this.getConnection(); // DB접속
+      String sql = "insert into member values(?,?,?,?,?,?,?,?,?,?,?,?,sysdate)";
+
+      try {
+         pstmt = conn.prepareStatement(sql);
+
+         // ?에 데이터 주입
+         pstmt.setString(1, memberDTO.getName());
+         pstmt.setString(2, memberDTO.getId());
+         pstmt.setString(3, memberDTO.getPwd());
+         pstmt.setString(4, memberDTO.getGender());
+         pstmt.setString(5, memberDTO.getEmail1());
+         pstmt.setString(6, memberDTO.getEmail2());
+         pstmt.setString(7, memberDTO.getTel1());
+         pstmt.setString(8, memberDTO.getTel2());
+         pstmt.setString(9, memberDTO.getTel3());
+         pstmt.setString(10, memberDTO.getZipcode());
+         pstmt.setString(11, memberDTO.getAddr1());
+         pstmt.setString(12, memberDTO.getAddr2());
+
+         su = pstmt.executeUpdate();// 실행 - 개수 리턴
+
+      } catch (SQLException e) {
+
+         e.printStackTrace();
+      } finally {
+         MemberDAO.close(conn, pstmt);
+      }
+
+      return su;
+   }
+
+   private static void close(Connection conn, PreparedStatement pstmt) {
+      try {
+         if (pstmt != null)
+            pstmt.close();
+         if (conn != null)
+            conn.close();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+
+   }
+
+}
--- workspace/memberServlet/src/main/java/member/service/WriteServlet.java
+++ workspace/memberServlet/src/main/java/member/service/WriteServlet.java
@@ -1,27 +1,82 @@
 package member.service;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import member.bean.MemberDTO;
+import member.dao.MemberDAO;
+
  
 //@WebServlet("/WriteServlet")
 public class WriteServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
        
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      request.setCharacterEncoding("UTF-8");
+      //데이터
+      
+      String name = request.getParameter("name");//이름 속성을 얻어온다. 
+      //이름, 아이디, 성별, 이메일2, 휴대폰3, 주소3
+      String id = request.getParameter("id");
+      String pwd = request.getParameter("pwd");
+      String gender = request.getParameter("gender");
+      String email1 = request.getParameter("email1");
+      String email2 = request.getParameter("email2");
+      String tel1 = request.getParameter("tel1");
+      String tel2 = request.getParameter("tel2");
+      String tel3 = request.getParameter("tel3");
+      String zipcode = request.getParameter("zipcode");
+      String addr1 = request.getParameter("addr1");
+      String addr2 = request.getParameter("addr2");
+   
+      
+      MemberDTO memberDTO = new MemberDTO();
+      memberDTO.setName(name);
+      memberDTO.setId(id);
+      memberDTO.setPwd(pwd);
+      memberDTO.setGender(gender);
+      memberDTO.setEmail1(email1);
+      memberDTO.setEmail2(email2);
+      memberDTO.setTel1(tel1);
+      memberDTO.setTel2(tel2);
+      memberDTO.setTel3(tel3);
+      memberDTO.setZipcode(zipcode);
+      memberDTO.setAddr1(addr1);
+      memberDTO.setAddr2(addr2);
+      
+      
+      //DB
+      MemberDAO memberDAO = MemberDAO.getInstance();   //클래스 생성
+      int su = memberDAO.memberWrite(memberDTO);
+      // memberDTO의 데이터를 memberDAO에 memberWrite로 넘긴 다음
+      // su를 리턴받아온다. 
+      
+      
+      //응답
+      response.setContentType("text/html; charset=UTF-8");
+      PrintWriter out = response.getWriter();//스트림 생성
+      out.println("<html>");
+      out.println("<body>");
+      
+      if(su == 0) {
+         out.println("<h3>회원가입 실패</h3>");
+      }
+      else {
+         out.println("<h3>회원가입 성공</h3>");
+      }
+      
+      out.println("</body>");
+      out.println("</html>");
+   }
+      
+      
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
-
+   
 }
